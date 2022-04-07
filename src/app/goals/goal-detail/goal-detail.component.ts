2c14e8461963bcc8ccd68632c14b0584d6e143c5
JosephatNgugi <Josephat.muriu@student.moringaschool.com>
2022-04-07 05:02:36 +0300

    Add a toggle function and a remove button when a task is complete.

--- src/app/goals/goal-detail/goal-detail.component.ts
+++ src/app/goals/goal-detail/goal-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Goal } from 'src/app/goal';
+// import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-goal-detail',
@@ -9,6 +10,11 @@ import { Goal } from 'src/app/goal';
 export class GoalDetailComponent implements OnInit {
 
   @Input() goal!: Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete)
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/goals/goals.component.ts
+++ src/app/goals/goals.component.ts
@@ -8,17 +8,23 @@ import { Goal } from '../goal';
 })
 export class GoalsComponent implements OnInit {
   goals: Goal[] = [
-    new Goal(
-      1,
-      'Watch finding Nemo',
-      'Find an online version and watch merlin find his son'
-    ),
+    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
     new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot'),
     new Goal(3, 'Get new Phone Case', 'Diana has her birthday coming up soon'),
     new Goal(4, 'Get Dog Food', 'Pupper likes expensive snacks'),
     new Goal(5, 'Solve math homework', 'Damn Math'),
     new Goal(6, 'Plot my world domination plan', 'Cause I am an evil overlord'),
   ];
+
+  toggleDetails(index: number){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+
+  completeGoal(isComplete: any, index: number){
+    if (isComplete) {
+      this.goals.splice(index, 1);
+    }
+  }
 
   constructor() {}
 
